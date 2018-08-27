d398e0ec1a4ad1c678ef3beeb17f52778824b83b
Jeremie <jeremie.puma@hotmail.fr>
2018-08-27 14:06:02 +0200

    Modification of SetPrice:
    if Price < 0 then Display 0
    Else if 0 < Price < 1000 the Display Price
    Else if Price > 1000 Display 1000

--- src/main/java/com/lambazon/domain/Product.java
+++ src/main/java/com/lambazon/domain/Product.java
@@ -63,7 +63,15 @@ public class Product {
 	}
 
 	public void setPrice(double price) {
-		this.price = price;
+
+	    if (price < 0)
+        this.price = 0;
+
+        else if (price > 1000)
+        this.price = 1000;
+
+	    else
+	        this.price = price;
 	}
 
 	public double getInventoryPrice() {
