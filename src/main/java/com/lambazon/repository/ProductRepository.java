5261b08fed715491a30f24dfa3e2389acbdad00e
Jeremie <jeremie.puma@hotmail.fr>
2018-08-27 13:21:17 +0200

    2 New items added to the list: - NewProduct1 - NewProduct2

--- src/main/java/com/lambazon/repository/ProductRepository.java
+++ src/main/java/com/lambazon/repository/ProductRepository.java
@@ -22,6 +22,8 @@ public class ProductRepository {
 		products.put(++id, new Product(id, 30, 69.99, "JVC HAFX8R Headphone", "Riptidz, In-Ear"));
 		products.put(++id, new Product(id, 40, 32.50, "VTech CS6114 DECT 6.0", "Cordless Phone"));
 		products.put(++id, new Product(id, 50, 895.00, "NOKIA OEM BL-5J", "Cell Phone "));
+		products.put(++id, new Product(id, 60, 100.00, "NewProduct1", "TestAddNewProduct1"));
+		products.put(++id, new Product(id, 70, 200.00, "NewProduct2", "TestAddNewProduct2"));
 	}
 	
 	public List<Product> products() {
